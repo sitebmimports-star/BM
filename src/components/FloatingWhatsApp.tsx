93ac4ecf55d3efa34c62aaa49b1d7b55efb3b3f0
sitebmimports-star <sitebmimports@gmail.com>
2026-06-17 12:49:38 +0000

    add tracking ids to whatsapp buttons

--- src/components/FloatingWhatsApp.tsx
+++ src/components/FloatingWhatsApp.tsx
@@ -3,7 +3,7 @@ import WhatsAppIcon from './WhatsAppIcon';
 
 export default function FloatingWhatsApp() {
   return (
-    <a
+    <a id="btn-whatsapp-floating"
       href="https://wa.me/5511976359458?text=Ol%C3%A1!%20Gostaria%20de%20falar%20com%20um%20especialista%20sobre%20as%20solu%C3%A7%C3%B5es%20em%20tecnologia%20e%20aparelhos%20dispon%C3%ADveis%20na%20BM%20Imports."
       target="_blank"
       rel="noopener noreferrer"
